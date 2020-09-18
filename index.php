16275d18df0e701261d73e71788f4173eaed89c9
Sanket Kulkarni <61157014+sanket-kulkarni-9168@users.noreply.github.com>
2020-09-19 00:03:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-	require 'dbconnect.php';
+	$servername = "localhost";
+	$database = "";
+	$username = "root";
+	$password = "";
+
+	$conn = mysqli_connect($servername,$username,$password,$database);
 	$querydb = mysqli_query($conn,"CREATE DATABASE android");
 	$querytable =mysqli_query($conn,"CREATE TABLE `android`.`users` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(20) NOT NULL , `email` VARCHAR(20) NOT NULL , `password` VARCHAR(20) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB");
 ?>
